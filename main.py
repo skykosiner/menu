9c41e19cc8eaf864c6d9d7f8a0045ab004f9e34a
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-21 09:17:53 +0000

    {order} woah math

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ order = menu.order()
 
 # If the item the user entered is not in the main menu
 if order not in mainMenu:
-    print("Sorry, we don't have that item.")
+    print(f"Sorry, we don't have {order} in the menu.")
 else:
     print("Your order has been placed.")
     time.sleep(0.5)
