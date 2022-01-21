4d594fb66e4a3626429e7bb219cd739bedcc357d
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-21 09:25:27 +0000

    nice

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ order = menu.order()
 # If the item the user entered is not in the main menu
 if order not in mainMenu:
     print(f"Sorry, we don't have {order} in the menu.")
-    exit(0)
+    exit(0x10F2C)
 
 # No errors then process that order baby
 print("Your order has been placed.")
