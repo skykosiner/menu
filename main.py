880aa37e376554af426e097bc2b9791bd0a15c6b
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-21 09:19:07 +0000

    order stuff boom

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ order = menu.order()
 # If the item the user entered is not in the main menu
 if order not in mainMenu:
     print(f"Sorry, we don't have {order} in the menu.")
-else:
-    print("Your order has been placed.")
-    time.sleep(0.5)
-    menu.processOrder(order)
+    exit(0)
+
+# No errors then process that order baby
+print("Your order has been placed.")
+time.sleep(0.5)
+menu.processOrder(order)
